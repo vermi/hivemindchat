3e9e7186850ea79b52093171fb6d9321de7c476d
Justin Vermillion <justin@lockboxsecurity.net>
2023-04-07 14:07:56 -0400

    implemented automated captioning

--- HiveMindChat/Helpers/ChatViewExtras.swift
+++ HiveMindChat/Helpers/ChatViewExtras.swift
@@ -25,28 +25,93 @@ extension ChatView {
     }
     
     func sendMessage() {
-        let userMessage = ChatMessage(role: .user, content: messageInput)
-        conversations[selectedConversationIndex].messages.append(IdentifiableChatMessage(chatMessage: userMessage))
-        scrollPublisher.send()
-        
-        DataManager.shared.saveConversationHistory(conversations) // Save the entire conversation history here
-        
+        guard !messageInput.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { return }
+
+        let newMessage = ChatMessage(role: .user, content: messageInput)
+        conversations[selectedConversationIndex].messages.append(IdentifiableChatMessage(chatMessage: newMessage))
         messageInput = ""
+
+        // Show typing indicator
         isTypingIndicatorVisible = true
-        
-        Task {
-            await chat(with: conversations[selectedConversationIndex].messages.map { $0.chatMessage }) { response in
-                DispatchQueue.main.async {
-                    conversations[selectedConversationIndex].messages.append(IdentifiableChatMessage(chatMessage: response))
-                    isTypingIndicatorVisible = false
-                    scrollPublisher.send()
-                    
-                    DataManager.shared.saveConversationHistory(conversations) // Save the entire conversation history here
+
+        // Check if the user has just sent their first message
+        if isFirstMessage() {
+            // Send user's message to the OpenAISwift API and receive the assistant's response
+            Task.init {
+                await chat(with: conversations[selectedConversationIndex].messages.map { $0.chatMessage }) { assistantMessage in
+                    DispatchQueue.main.async {
+                        // Add the assistant's response to the conversation
+                        conversations[selectedConversationIndex].messages.append(IdentifiableChatMessage(chatMessage: assistantMessage))
+                        DataManager.shared.saveConversationHistory(conversations)
+                        scrollPublisher.send()
+
+                        // Hide typing indicator
+                        isTypingIndicatorVisible = false
+
+                        // Generate the caption
+                        generateCaption()
+                    }
+                }
+            }
+        } else {
+            // Only send user's message and receive the assistant's response
+            Task.init {
+                await chat(with: conversations[selectedConversationIndex].messages.map { $0.chatMessage }) { assistantMessage in
+                    DispatchQueue.main.async {
+                        // Add the assistant's response to the conversation
+                        conversations[selectedConversationIndex].messages.append(IdentifiableChatMessage(chatMessage: assistantMessage))
+                        DataManager.shared.saveConversationHistory(conversations)
+                        scrollPublisher.send()
+
+                        // Hide typing indicator
+                        isTypingIndicatorVisible = false
+                    }
                 }
             }
         }
     }
     
+    func isFirstMessage() -> Bool {
+        let userMessages = conversations[selectedConversationIndex].messages.filter { $0.chatMessage.role == .user }
+        return userMessages.count == 1
+    }
+    
+    func generateCaption() {
+        // Filter out system messages and the first assistant message
+        let relevantMessages = conversations[selectedConversationIndex].messages
+            .enumerated()
+            .filter { (index, message) in
+                if message.chatMessage.role == .assistant && index == 1 {
+                    return false
+                }
+                return message.chatMessage.role != .system
+            }
+            .map { $0.element }
+
+        // Concatenate the contents of the filtered messages
+        let conversationContent = relevantMessages
+            .map { $0.chatMessage.content }
+            .joined(separator: " ")
+
+        // Create a chat message for the caption prompt
+        let promptMessage = ChatMessage(role: .system, content: "The main topic of the following conversation is: \(conversationContent). Generate a concise and easy-to-understand title that summarizes the main topic of this conversation.")
+
+        // Send the chat message to the OpenAISwift API
+        Task.init {
+            await chat(with: [promptMessage]) { captionMessage in
+                var caption = captionMessage.content.trimmingCharacters(in: .whitespacesAndNewlines)
+                caption = caption.trimmingCharacters(in: CharacterSet(charactersIn: "\""))
+
+                DispatchQueue.main.async {
+                    // Set the conversation title.
+                    conversations[selectedConversationIndex].title = caption
+                    // Save conversation with the updated title.
+                    DataManager.shared.saveConversationHistory(conversations)
+                }
+            }
+        }
+    }
+
     func loadChatHistory() {
         let chatHistory = conversations[selectedConversationIndex].loadChatHistory()
         if !chatHistory.isEmpty {
